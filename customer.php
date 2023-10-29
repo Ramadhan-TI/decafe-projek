ddfeb5a684ad64cfc4196da1f96871b02c8d7bdb
ramadhan <mhdrama405@gmail.com>
2023-10-29 17:57:21 +0700

    decafe projek

---
+++ customer.php
@@ -0,0 +1,14 @@
+            <div class="col-lg-9 mt-2">
+                <div class="card">
+                    <div class="card-header">
+                        customer
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Ini bagian customer</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content. orem
+                            Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                            the industry's standard dummy text ever since the 1500s</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
---
+++ proses/connect.php
@@ -0,0 +1,6 @@
+<?php
+    $conn = mysqli_connect("localhost","u837952942_ramadhan","8yPKnAz@","u837952942_ramadhan");
+    if(!$conn){
+        echo "Gagal koneksi";
+    }
+?>
